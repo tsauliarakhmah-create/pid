4a6ed4ca2392e901647786501f648bc344dd0a2f
Tsabita <tsauliarakhmah@gmail.com>
2026-03-09 21:25:55 +0700

    hai

---
+++ NAMA_NRP.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama, nrp;
+    nama = "Tsabita Aulia Rakhmah";
+    nrp = "5026251129";
+
+    cout << "My Name" << nama << endl;
+    cout << "NRP" << nrp << endl;
+    return 0;
+}
